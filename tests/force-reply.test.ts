df892ad87e29c36cd87c77018d93f2b349172e78
Kravets <57632712+kravetsone@users.noreply.github.com>
2024-02-04 01:42:26 +0300

    chore: add ForceReplyKeyboard tests

---
+++ tests/force-reply.test.ts
@@ -0,0 +1,22 @@
+import { describe, expect, test } from "bun:test";
+import { ForceReplyKeyboard } from "../src";
+
+describe("ForceReplyKeyboard", () => {
+	test("simple", () => {
+		expect(new ForceReplyKeyboard().toJSON()).toEqual({
+			force_reply: true,
+			selective: false,
+			input_field_placeholder: undefined,
+		});
+	});
+
+	test("with options", () => {
+		expect(
+			new ForceReplyKeyboard().selective().placeholder("GRAMIO").toJSON(),
+		).toEqual({
+			force_reply: true,
+			selective: true,
+			input_field_placeholder: "GRAMIO",
+		});
+	});
+});
