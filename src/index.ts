bd86771865b728525e2a898e6f942ad2118bff22
Kravets <57632712+kravetsone@users.noreply.github.com>
2024-02-04 14:53:15 +0300

    chore: add keyboard postfix for force reply

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import "reflect-metadata";
 export * from "./keyboard";
 export * from "./inline-keyboard";
 export * from "./remove-keyboard";
-export * from "./force-reply";
+export * from "./force-reply-keyboard";
 export * from "./base-keyboard-constructor";
