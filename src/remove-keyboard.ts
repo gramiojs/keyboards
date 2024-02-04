e821592f1d79640a95e4a81ba5ba857f205ab41b
Kravets <57632712+kravetsone@users.noreply.github.com>
2024-02-04 19:50:41 +0300

    chore: add JSDoc examples for RemoveKeyboard

--- src/remove-keyboard.ts
+++ src/remove-keyboard.ts
@@ -20,6 +20,11 @@ export class RemoveKeyboard {
 	 * Use this parameter if you want to remove the keyboard for specific users only. Targets: 1) users that are \@mentioned in the *text* of the [Message](https://core.telegram.org/bots/api/#message) object; 2) if the bot's message is a reply to a message in the same chat and forum topic, sender of the original message.
 	 *
 	 * *Example:* A user votes in a poll, bot returns confirmation message in reply to the vote and removes the keyboard for that user, while still showing the keyboard with poll options to users who haven't voted yet.
+	 * @example
+	 * ```ts
+	 * new RemoveKeyboard().selective(); // to enable
+	 * new RemoveKeyboard().selective(false); // to disable
+	 * ```
 	 */
 	selective(isEnabled = true) {
 		this.options.isSelective = isEnabled;
